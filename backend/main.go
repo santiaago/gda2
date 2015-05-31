384b8acc7d78f952ab19f2eea1b81601d1845494
santiaago <santiago.ariassar@gmail.com>
2015-05-31 19:32:26 +0100

    add hello world handler

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func main() {
+	http.HandleFunc("/api/hello", helloWorld)
 	http.ListenAndServe(":8080", nil)
+}
+
+// helloWorld handler returns a json response with a hello world message.
+//
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		Message string
+	}{
+		"hello world",
+	}
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Println(err)
+	}
 }
